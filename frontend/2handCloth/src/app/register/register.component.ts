1a06d3a378ce064783ccf7c14856a378fddd3f86
PauloBrandao <paulo.brandao@visualnuts.com>
2023-02-27 23:35:09 +0000

    create register and login

---
+++ frontend/2handCloth/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  
+  constructor(private formBuilder: FormBuilder, public registrationForm: FormGroup) {}
+  ngOnInit(): void {
+    this.registrationForm = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ['', Validators.required],
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(formGroup: any) {
+    const password = formGroup.get('password').value;
+    const confirmPassword = formGroup.get('confirmPassword').value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
+  }
+}
