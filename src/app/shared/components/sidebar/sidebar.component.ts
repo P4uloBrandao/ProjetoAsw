2617db1d4a844479f2be6a2473a861269a1a52e9
PauloBrandao <paulo.brandao@visualnuts.com>
2023-04-20 13:54:21 +0100

    names

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -9,8 +9,15 @@ import { Router } from '@angular/router';
 export class SidebarComponent {
   @Input() MENUS : any;
   @Input() AUTHENTICATED : boolean = false;
+  public name : any;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) { 
+    if (localStorage.getItem('currentUser')) {
+      this.name = JSON.parse(localStorage.getItem('currentUser')!).name;
+    } else {
+      this.name = null;
+    }
+  }
 
   public logout () {
     localStorage.removeItem('currentUser');
